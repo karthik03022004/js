ee885c5bb7c163a5c832e6840ae40d6a444d8341
karthik03022004 <karthiksunny222@gmail.com>
2025-09-01 22:55:02 +0530

    bulb on off

---
+++ script.js
@@ -0,0 +1,6 @@
+function bulbaction(){
+   document.getElementById("bulb").src = "https://dam.thdstatic.com/content/production/ruF-hrAfxNxA7neeS0lorA/LssEKFfLvClWIi3yuA7c0w/Original%20file/02FEB23_LightBulbs_VN_150Watt_02.png?im=Resize=(703,703)";
+}
+function bulboff(){
+    document.getElementById("bulb").src= "https://thumbs.dreamstime.com/b/d-illustration-empty-light-bulb-isolated-white-background-realistic-d-rendering-electric-lamp-inside-d-103349315.jpg";
+}
