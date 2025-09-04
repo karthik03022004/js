b8aaa0dc53e5174673beb73736a97bd285a5d570
karthik03022004 <karthiksunny222@gmail.com>
2025-09-05 00:03:11 +0530

    colorpick

---
+++ colorpick/script.js
@@ -0,0 +1,28 @@
+let gr=document.getElementById("green")
+gr.style.backgroundColor="green"
+gr.style.color="black"
+
+let ye=document.getElementById("yellow")
+ye.style.backgroundColor="yellow"
+ye.style.color="black"
+let p=document.getElementById("purple")
+p.style.backgroundColor="purple"
+p.style.color="black"
+let i=document.getElementById("pink")
+i.style.backgroundColor="pink"
+i.style.color="black"
+
+document.body.style.backgroundColor="black"
+
+function green(){
+    document.body.style.backgroundColor="green"
+}
+function yellow(){
+    document.body.style.backgroundColor="yellow"
+}
+function purple(){
+    document.body.style.backgroundColor="purple"
+}
+function pink(){
+    document.body.style.backgroundColor="pink"
+}
