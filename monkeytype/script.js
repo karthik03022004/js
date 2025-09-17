aa102fd2a40c8b6f9c3c00d94ec893e16a255e49
karthik03022004 <karthiksunny222@gmail.com>
2025-09-17 23:21:26 +0530

    modified

--- monkeytype/script.js
+++ monkeytype/script.js
@@ -4,7 +4,6 @@ let format_sen=sentence_ele.textContent.split("").map(function(each){
     return `<span>${each}</span>`
 })
 sentence_ele.innerHTML=format_sen.join("")
-
 let all_span=document.querySelectorAll("span")
 let user_ele=document.getElementById("user_input")
 user_ele.addEventListener('keyup',function(){
@@ -12,7 +11,6 @@ user_ele.addEventListener('keyup',function(){
     for(let i=0;i<cur_val.length;i++){
         if(cur_val[i]==all_span[i].textContent){
                 all_span[i].style.color='green'
-
             }
             else{
                 all_span[i].style.color='red'
@@ -24,14 +22,15 @@ user_ele.addEventListener('keyup',function(){
     
 })
 let time_ele=document.getElementById("timing")
-num=13
+num=30
 let stoper=setInterval(function(){
     time_ele.textContent="time left:"+num
     num-=1
     time_ele.style.color="green"
     if(num<0){
         clearInterval(stoper)
-        sentence_ele.textContent="time up"
+        sentence_ele.textContent="time up ! "
+        user_ele.value=""
     }
     if(num<10){
         time_ele.style.color="red"
